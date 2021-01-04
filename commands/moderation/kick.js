5908df289ebc6c772a408047fb179fef2d44cec0
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2021-01-04 17:14:21 +0000

    Kick Command

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -15,13 +15,34 @@ module.exports = class KickCommand extends Command {
                     key: 'user',
                     prompt: 'What user would you like to kick?',
                     type: 'string'
+                },
+
+                {
+                    key: 'reason',
+                    prompt: 'What is the reason you would like to kick this user for?',
+                    type: "string",
+                    default: ""
                 }
             ]
         });
     }
 
-    async run(message, { text }) {
-        let member = message.guild.members.cache.filter(user => user.id === text);
-        message.say(member.name);
+    async run(message, { user }, { reason }) {
+        let userA = this.getUserByName(message, user);
+        let member = message.guild.member(userA);
+        if (member.kickable) {
+            member.kick(reason);
+            message.channel.send(`${userA.name} has been kicked from the server! Pog champ!`);
+        }
+        else message.channel.send("I cannot kick this user!");
+    }
+
+    getUserByName(message, name) {
+        const user = this.client.users.cache.find(user => user.username == name) || message.guild.members.cache.get(name) || message.mentions.members.first();
+        if (!user) {
+            console.log(`Could not find ${name} in the cache`);
+            return;
+        }
+        return user;
     }
 }
